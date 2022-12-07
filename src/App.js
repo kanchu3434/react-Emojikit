e5b8d4e8e45080377e39d652d386c4933abc03a2
kanchan <kanchankotkar001@gmail.com>
2022-12-07 16:44:29 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [emoji,setEmoji]=useState("🏆")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <>
+   <div className='container'>
+          😍Emoji Kit😍 
+   </div>
+
+   <div className='main-emoji-container'>
+         <div className='main-emoji'>
+         {emoji}
+         </div>
+  
+   </div>
+   <div className='emoji-container'> 
+     <div className='emoji-item'onClick={()=>{setEmoji('🏆')}}>🏆</div>
+      <div className='emoji-item'onClick={()=>{setEmoji('✌')}}>✌</div>
+        <div className='emoji-item'onClick={()=>{setEmoji('😊')}}>😊</div>
+       <div className='emoji-item'onClick={()=>{setEmoji('😻')}}>😻</div>
+      <div className='emoji-item'onClick={()=>{setEmoji('🍰')}}>🍰</div>
     </div>
+   </>
   );
 }
 
